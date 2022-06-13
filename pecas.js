b53e7f3215b8d2706a8c179c04db42c31b6dc161
tamirescalixto <tamires.americo.calixto@gmail.com>
2022-06-13 18:58:48 -0300

    Atividade (Encontro Remoto 1)

---
+++ pecas.js
@@ -0,0 +1,22 @@
+let pesoPeca = 125
+let numeroPecas = 7
+let nomePeca = "Roda"
+
+if(pesoPeca > 100) {
+    console.log("Pode-se cadastrar a peça!")
+} else {
+    console.log("Peça com peso insuficiente!")
+}
+
+if (numeroPecas >= 10) {
+    console.log("Impossível cadastrar, número de peças excedido!")
+} else {
+    console.log("Cadastro permitido!")
+}
+
+if (nomePeca.length < 3) {
+    console.log("Número de caracteres insuficiente")
+} else {
+    console.log("Nome adequado de acordo com as diretivas!")
+}
+
